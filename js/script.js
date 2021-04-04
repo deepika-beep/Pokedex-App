b41ca92863f0fb3533aec540e4417035cb0ae91b
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-04-04 23:59:33 +0200

    added objects and three keys to each Pokémon object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // This array contains Pokémon data to display in your application.
 
-let pokemonList=[];
+let pokemonList=[{name:'Bulbasaur',height:'7',types['grass', 'poison']},
+{name:'Ivysaur',height:'1',types['grass', 'monster']}],
+{name:'Venusaur',height:'2',types['grass', 'monster']}
+;
