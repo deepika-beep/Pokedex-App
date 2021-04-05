bc5412cb6b8a8df1c718ccc1e33267c87b74b88c
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-04-05 11:18:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // This array contains Pokémon data to display in your application.
 
-let pokemonList=[{name:'Bulbasaur',height:'7',types['grass', 'poison']},
-{name:'Ivysaur',height:'1',types['grass', 'monster']}],
-{name:'Venusaur',height:'2',types['grass', 'monster']}
-;
+let pokemonList=[{name:'Bulbasaur',height:'7',types:['grass', 'poison']},
+{name:'Ivysaur',height:'1',types:['grass', 'monster']},
+{name:'Venusaur',height:'2',types:['grass', 'monster']}
+];
