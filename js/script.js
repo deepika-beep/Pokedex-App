ca591751f6ec87954367e333ba9459194170474f
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-04-06 15:26:14 +0200

    deleted one for loop

--- js/script.js
+++ js/script.js
@@ -5,21 +5,20 @@ let pokemonList=[{name:'Bulbasaur',height:'7',types:['grass', 'poison']},
 {name:'Venusaur',height:'2',types:['grass', 'monster']}
 ];
 // display the Pokémon name and height on  website’s DOM
-for(i=0;i<pokemonList.length;i++)
-{
-  document.write(pokemonList[i].name +  " height: " + pokemonList[i].height);
-}
+
 // Added The conditional to check if the height is above a certain value to print the following output
 for(i=0;i<pokemonList.length;i++)
+{
 if(pokemonList[i].height>3)
 {
 
-  document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " Wow, that’s big!");
+  document.write(pokemonList[i].name + "(" + " height: "  + pokemonList[i].height + ")" + " Wow, that’s big!" + "<br>");
 }
 else if(pokemonList[i].height==3)
 {
-  document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " that’s average!");
+  document.write(pokemonList[i].name + "(" + " height: " + pokemonList[i].height + ")"+ " that’s average!" + "<br>");
 }
 else{
-document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " that’s small!");
+document.write(pokemonList[i].name + "(" +" height: " + pokemonList[i].height + ")" +" that’s small!" + "<br>");
+}
 }
