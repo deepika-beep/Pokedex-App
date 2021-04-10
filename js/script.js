d446ae6b414e7109ab33bcdb34afb80d99bd7016
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-04-10 15:37:35 +0200

    Used forEach loop instead of for loop

--- js/script.js
+++ js/script.js
@@ -7,18 +7,23 @@ let pokemonList=[{name:'Bulbasaur',height:'7',types:['grass', 'poison']},
 // display the Pokémon name and height on  website’s DOM
 
 // Added The conditional to check if the height is above a certain value to print the following output
-for(i=0;i<pokemonList.length;i++)
-{
-if(pokemonList[i].height>3)
-{
+// for(i=0;i<pokemonList.length;i++)
+// {
+// if(pokemonList[i].height>3)
+// {
+//
+//   document.write(pokemonList[i].name + "(" + " height: "  + pokemonList[i].height + ")" + " Wow, that’s big!" + "<br>");
+// }
+// else if(pokemonList[i].height==3)
+// {
+//   document.write(pokemonList[i].name + "(" + " height: " + pokemonList[i].height + ")"+ " that’s average!" + "<br>");
+// }
+// else{
+// document.write(pokemonList[i].name + "(" +" height: " + pokemonList[i].height + ")" +" that’s small!" + "<br>");
+// }
+// }
 
-  document.write(pokemonList[i].name + "(" + " height: "  + pokemonList[i].height + ")" + " Wow, that’s big!" + "<br>");
-}
-else if(pokemonList[i].height==3)
-{
-  document.write(pokemonList[i].name + "(" + " height: " + pokemonList[i].height + ")"+ " that’s average!" + "<br>");
-}
-else{
-document.write(pokemonList[i].name + "(" +" height: " + pokemonList[i].height + ")" +" that’s small!" + "<br>");
-}
-}
+// Used a forEach() function instead of the for loop to iterate over the Pokémon in pokemonList array to print the details of each one.
+pokemonList.forEach(function(pokemon){
+  document.write('name:'+ pokemon.name + ' height:' + pokemon.height +' types:'+ pokemon.types + "</br>");
+});
