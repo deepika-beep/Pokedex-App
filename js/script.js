15977ffa2089f75d9c354b3cca870fa98e741e5a
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-04-10 16:50:44 +0200

    Used getAll function to retrieve the pokemonList array.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 // This array contains Pokémon data to display in your application.
-
+// Created a new pokemonRepository variable to hold what the  IIFE will return.
+let  pokemonRepository =(function(){
 let pokemonList=[{name:'Bulbasaur',height:'7',types:['grass', 'poison']},
 {name:'Ivysaur',height:'3',types:['grass', 'monster']},
 {name:'Venusaur',height:'2',types:['grass', 'monster']}
 ];
+function getAll(){
+  return pokemonList;
+}
+function add(item){
+pokemonList.push(item);
+}
+return{
+  getAll:getAll,
+  add:add
+};
+})();
+
+pokemonRepository.add({name:'Charizard',types:['fire','flying']});
+console.log(pokemonRepository.getAll());
 // display the Pokémon name and height on  website’s DOM
 
 // Added The conditional to check if the height is above a certain value to print the following output
@@ -24,6 +39,6 @@ let pokemonList=[{name:'Bulbasaur',height:'7',types:['grass', 'poison']},
 // }
 
 // Used a forEach() function instead of the for loop to iterate over the Pokémon in pokemonList array to print the details of each one.
-pokemonList.forEach(function(pokemon){
-  document.write('name:'+ pokemon.name + ' height:' + pokemon.height +' types:'+ pokemon.types + "</br>");
-});
+pokemonRepository.getAll().forEach(function(item){
+  document.write('name:'+ item.name + ' height:' + item.height +' types:'+ item.types + "</br>");
+ });
